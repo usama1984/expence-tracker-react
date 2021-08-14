ee8c73e43adfff09a6a386539bdf06c704459832
usama <usamamuhammad231@gmail.com>
2021-08-14 14:39:10 +0500

    expence tracker app

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+import  {Header}  from "./components/Header"
+import { Balance } from "./components/Balance"
+import { IncomeExpense } from "./components/IncomeExpense"
+import { TransactionList } from "./components/TransactionList"
+import { AddTransaction } from "./components/AddTransaction"
+
+import {GlobalProvider } from './context/GlobalState'
+
+function App() {
+  return (
+    <div className="app">
+      <GlobalProvider>
+        <Header />
+        <div className="container">
+        <Balance />
+        <IncomeExpense />
+        <TransactionList />
+        <AddTransaction />
+        </div>
+      </GlobalProvider>
+    </div>
+  );
+}
+
+export default App;
